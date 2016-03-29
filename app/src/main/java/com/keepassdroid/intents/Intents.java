5e37eced289d50b8a0fb4b3ae29f338642170f25
Maxime Flasquin <maxime.flasquin@gmail.com>
2016-03-29 16:12:40 +0200

    Génération d'un QRCode pour éviter la copie du mot de passe

--- app/src/main/java/com/keepassdroid/intents/Intents.java
+++ app/src/main/java/com/keepassdroid/intents/Intents.java
@@ -20,9 +20,10 @@
 
 public class Intents {
 	public static final String TIMEOUT = "com.keepassdroid.timeout";
-	
+
 	public static final String COPY_USERNAME = "com.keepassdroid.copy_username";
 	public static final String COPY_PASSWORD = "com.keepassdroid.copy_password";
+	public static final String SHOW_QRCODE = "com.keepassdroid.show_qrcode";
 
 	public static final String OPEN_INTENTS_FILE_BROWSE = "org.openintents.action.PICK_FILE";
 }
