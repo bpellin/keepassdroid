be05d82816306d24cabe756e86c5fef40b23d71b
Brian Pellin <bpellin@gmail.com>
2017-01-07 01:21:02 -0600

    Attribute initial fingerprint implementation.

---
+++ app/src/main/java/com/keepassdroid/compat/KeyPropertiesCompat.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017 Brian Pellin.
+ *
+ * This file is part of KeePassDroid.
+ *
+ *  KeePassDroid is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  KeePassDroid is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePassDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.keepassdroid.compat;
+
+public class KeyPropertiesCompat {
+    public static final String KEY_ALGORITHM_AES = "AES";
+    public static final String BLOCK_MODE_CBC = "CBC";
+    public static final String ENCRYPTION_PADDING_PKCS7 = "PKCS7Padding";
+    public static final int ENCRYPT = 1;
+    public static final int DECRYPT = 2;
+
+}
