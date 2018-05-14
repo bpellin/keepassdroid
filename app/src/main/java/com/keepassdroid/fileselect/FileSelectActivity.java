50ad5d3f333178c2a37830e052afcc40919d953e
Brian Pellin <bpellin@gmail.com>
2018-05-13 23:28:51 -0500

    Remove extraneous permission code

--- app/src/main/java/com/keepassdroid/fileselect/FileSelectActivity.java
+++ app/src/main/java/com/keepassdroid/fileselect/FileSelectActivity.java
@@ -416,9 +416,6 @@ public class FileSelectActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 
-		// check for storage permission
-		checkStoragePermission();
-		
 		// Check to see if we need to change modes
 		if ( fileHistory.hasRecentFiles() != recentMode ) {
 			// Restart the activity
@@ -429,60 +426,6 @@ public class FileSelectActivity extends Activity {
 		
 		FileNameView fnv = (FileNameView) findViewById(R.id.file_select);
 		fnv.updateExternalStorageWarning();
-	}
-
-	private void checkStoragePermission() {
-		// Here, thisActivity is the current activity
-		if (ContextCompat.checkSelfPermission(FileSelectActivity.this,
-											  Manifest.permission.WRITE_EXTERNAL_STORAGE)
-				!= PackageManager.PERMISSION_GRANTED) {
-
-			// Should we show an explanation?
-			//if (ActivityCompat.shouldShowRequestPermissionRationale(FileSelectActivity.this,
-			//														Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-				// Show an explanation to the user *asynchronously* -- don't block
-				// this thread waiting for the user's response! After the user
-				// sees the explanation, try again to request the permission.
-
-			//} else {
-
-				// No explanation needed, we can request the permission.
-
-				ActivityCompat.requestPermissions(FileSelectActivity.this,
-												  new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-												  MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE);
-
-				// MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-				// app-defined int constant. The callback method gets the
-				// result of the request.
-			//}
-		}
-	}
-
-	@Override
-	public void onRequestPermissionsResult(int requestCode,
-										   String permissions[], int[] grantResults) {
-		switch (requestCode) {
-			case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE: {
-				// If request is cancelled, the result arrays are empty.
-				if (grantResults.length > 0
-						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-					// permission was granted, yay! Do the
-					// contacts-related task you need to do.
-
-				} else {
-
-					// permission denied, boo! Disable the
-					// functionality that depends on this permission.
-				}
-				return;
-			}
-
-			// other 'case' lines to check for other
-			// permissions this app might request
-		}
 	}
 
 	@Override
