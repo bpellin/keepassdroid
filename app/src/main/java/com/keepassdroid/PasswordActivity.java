a030481e42fa1172d0fd6fd79e91c9b36c5c9e06
Brian Pellin <bpellin@gmail.com>
2020-02-13 23:14:38 -0600

    improve invalidation handling

--- app/src/main/java/com/keepassdroid/PasswordActivity.java
+++ app/src/main/java/com/keepassdroid/PasswordActivity.java
@@ -26,6 +26,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 
 import com.android.keepass.R;
 import com.keepassdroid.app.App;
@@ -83,5 +84,8 @@ public class PasswordActivity extends LockingActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
